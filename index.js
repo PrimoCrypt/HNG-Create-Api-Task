b2297a1668fa5d0f42c2fb1360aadd1e33a4376a
PrimoCrypt <yhungdew@gmail.com>
2022-11-01 23:40:41 +0100

    First Commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var express = require("express");
-var app = express();
+const express = require('express')
+const dotenv = require('dotenv')
 
-app.get("/", (req, res) => {
-    res.json({"slackUsername": 'LeoNesi', "backend": true, "age": 21, "bio": 'I am a full-stack Developer hoping to expand skills and impact on the society'});
-});
+dotenv.config();
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+const port = process.env.PORT || 5000
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({
+        "slackUsername": 'LeoNesi',
+        "backend": true,
+        "age": 21,
+        "bio": 'I am a full-stack Developer hoping to expand skills and impact on the society'
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
