cc7778c88db759be8e98ca0c2cd8a380e62eb4d8
PrimoCrypt <yhungdew@gmail.com>
2022-11-01 22:26:02 +0100

    removed next from the augment in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     res.json({"slackUsername": 'LeoNesi', "backend": true, "age": 21, "bio": 'I am a full-stack Developer hoping to expand skills and impact on the society'});
 });
 
